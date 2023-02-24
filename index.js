bfce6b5d966bb1c1b040ef5d283ed8aaf46af3c5
samyrsilva <54744370+samyrsilva@users.noreply.github.com>
2023-02-24 20:45:45 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,23 @@
-/Com o do:
-let listaDeEstudantes = ["Helena", "Chico", "Mário"];
-let quantidadeDeEstudantes = listaDeEstudantes.length;
-let inicio = 0;
-
-do { 
-    console.log(listaDeEstudantes[inicio]);  
-    inicio++;  
-} while (inicio < quantidadeDeEstudantes);  
-   
-//------------------------------------------------//  
-   
-//Com o while:  
-while (inicio < quantidadeDeEstudantes) {  
-     console.log(listaDeEstudantes[inicio]);  
-     inicio++;  
-}
+HTML>
+<HEAD><TITLE>ATIVIDADE DE VERSIONAMENTO</TITLE></HEAD>
+<BODY>
+   <H1> TÍTULO1 </H1>
+</BODY>
+</HTML>
+- publicar seu arquivo na branch main;
+- criar uma nova branch, chamada feature1;
+- na branch feature1, publicar o mesmo arquivo;
+- realizar seguinte alteração na linha h1 do arquivo da branch main:
+<HTML>
+<HEAD><TITLE>ATIVIDADE DE VERSIONAMENTO</TITLE></HEAD>
+<BODY>
+   <H1> VERSIONAMENTO </H1>
+</BODY>
+</HTML>
+- realizar seguinte alteração na linha h1 do arquivo da branch feature1:
+<HTML>
+<HEAD><TITLE>ATIVIDADE DE VERSIONAMENTO</TITLE></HEAD>
+<BODY>
+   <H1> GIT </H1>
+</BODY>
+</HTML>
