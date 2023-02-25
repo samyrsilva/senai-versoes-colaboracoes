be718e489429a7f8650cd7ef85bd0e9fe281228e
samyrsilva <54744370+samyrsilva@users.noreply.github.com>
2023-02-24 22:07:28 -0300

    Update controle de estoque.js

--- controle de estoque.js
+++ controle de estoque.js
@@ -1,47 +1,33 @@
-import java.util.Scanner;
+console.log("Controle de Estoque");
 
-public class ControleEstoque {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        
-        System.out.println("Controle de Estoque");
-        
-        // solicita o saldo inicial da peça
-        System.out.print("Digite o saldo inicial da peça: ");
-        int saldo = sc.nextInt();
-        System.out.println("Saldo inicial: " + saldo);
-        
-        // executa operações enquanto o usuário quiser continuar
-        int operacao = 0;
-        while (operacao != 3) {
-            System.out.println("\nSelecione a operação:");
-            System.out.println("1 - Compra (entrada) de peças");
-            System.out.println("2 - Venda (saída) de peças");
-            System.out.println("3 - Encerrar o programa");
-            System.out.print("Operação selecionada: ");
-            operacao = sc.nextInt();
-            
-            if (operacao == 1) { // compra
-                System.out.print("Digite a quantidade de peças compradas: ");
-                int quantidade = sc.nextInt();
-                saldo += quantidade;
-                System.out.println("Saldo atualizado: " + saldo);
-            } else if (operacao == 2) { // venda
-                System.out.print("Digite a quantidade de peças vendidas: ");
-                int quantidade = sc.nextInt();
-                if (quantidade <= saldo) {
-                    saldo -= quantidade;
-                    System.out.println("Saldo atualizado: " + saldo);
-                } else {
-                    System.out.println("Saldo insuficiente para a venda!");
-                }
-            } else if (operacao == 3) { // encerrar
-                System.out.println("Encerrando o programa...");
-            } else { // operação inválida
-                System.out.println("Operação inválida!");
-            }
+// solicita o saldo inicial da peça
+let saldo = parseInt(prompt("Digite o saldo inicial da peça: "));
+console.log("Saldo inicial: " + saldo);
+
+// executa operações enquanto o usuário quiser continuar
+let operacao = 0;
+while (operacao != 3) {
+    console.log("\nSelecione a operação:");
+    console.log("1 - Compra (entrada) de peças");
+    console.log("2 - Venda (saída) de peças");
+    console.log("3 - Encerrar o programa");
+    operacao = parseInt(prompt("Operação selecionada: "));
+    
+    if (operacao == 1) { // compra
+        let quantidade = parseInt(prompt("Digite a quantidade de peças compradas: "));
+        saldo += quantidade;
+        console.log("Saldo atualizado: " + saldo);
+    } else if (operacao == 2) { // venda
+        let quantidade = parseInt(prompt("Digite a quantidade de peças vendidas: "));
+        if (quantidade <= saldo) {
+            saldo -= quantidade;
+            console.log("Saldo atualizado: " + saldo);
+        } else {
+            console.log("Saldo insuficiente para a venda!");
         }
-        
-        sc.close();
+    } else if (operacao == 3) { // encerrar
+        console.log("Encerrando o programa...");
+    } else { // operação inválida
+        console.log("Operação inválida!");
     }
 }
